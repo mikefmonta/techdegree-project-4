c36c7d168b3838959b4ab2394ed54df1113fbdb1
Michael Montalvo <powertreex@gmail.com>
2017-12-04 16:01:52 -0800

    Got everything to work.  Went for the extra credit. Did the mobile site, re-wrote a lot of the structure of the html. Got the JS to work.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,27 @@
-$("#searchInput").keyup(function(search){
-    var $captions = document.querySelectorAll("data-title");
-    var $image = ".row img";
-    if ( search.toLowerCase === $captions.toLowerCase ) {
-      $image.show;
-    } else {
-      $image.hide;
-    }
-  });
+$(document).ready(function() {
+
+    /* Variables */
+    var $gallery = $('#gallery');
+    var $search = $('input');
+
+    /* Key based events */
+
+    //keyup event to run filter function
+    $search.on('keyup', function() {
+        //store the search input
+        var search = $(this).val().toLowerCase();
+
+        // for each gallery item, get the items to filter
+        $gallery.children('a').each(function(index, item) {
+
+            //filter based on image caption
+            if ($(item).filter('[data-title *= ' + search + ']').length > 0 || search.length < 1) {
+                $(item).show().addClass('active');
+            } else {
+                $(item).hide().removeClass('active');
+            }
+        });
+        //create array from items that are visible
+        populateArray();
+    });
+});
